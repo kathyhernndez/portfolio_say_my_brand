7595f4bf1bc4e06e097751254f9c5a041cf86146
Katherine Hernandez <kathernndez97@gmail.com>
2021-12-06 17:55:52 -0400

    reentrenamiento del chatbot

--- chat_bot/chat.py
+++ chat_bot/chat.py
@@ -45,7 +45,7 @@ def get_response(msg):
             if tag == intent["tag"]:
                 return random.choice(intent['responses'])
     
-    return "I do not understand..."
+    return "Lo siento no entiendo..."
 
 
 if __name__ == "__main__":
