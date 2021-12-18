5c964585aa7601bc90d5631c049d333a2e5bdc6c
Jesús Monge <jesus_jaml@outlook.com>
2021-12-17 19:57:05 -0500

    Booking Component created and imported in the index.js file

---
+++ Components/Booking.js
@@ -0,0 +1,14 @@
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
+
+
+const Booking = () => {
+    return (
+        <div>
+            {/* <DateRangePicker/> */}
+        </div>
+    )
+}
+
+export default Booking
