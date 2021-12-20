833c8209d2cdca94f0d01dab9d9568ade5dfd1d6
Jesús Monge <jesus_jaml@outlook.com>
2021-12-20 01:22:23 -0500

    Search Component in the pages folder. Patch /search. Here we weill show the Rooms

---
+++ pages/search.js
@@ -0,0 +1,16 @@
+import Header from "../Components/Header"
+
+const Search = () => {
+    return (
+        <div>
+            <Header/>
+            <main>
+                <section>
+                    <h1 className="text-center mt-5 font-bold text-purple-800 text-2xl">Selecciona Tu Habitación</h1>
+                </section>
+            </main>
+        </div>
+    )
+}
+
+export default Search
