9bd53c9661f925964fcba8d1421493bc375c8d78
Jesús Monge <jesus_jaml@outlook.com>
2021-12-20 01:20:54 -0500

    Booking Form 2.0. Number of guests and Cancel button working.

--- Components/Booking.js
+++ Components/Booking.js
@@ -1,20 +1,17 @@
 import { useState } from "react";
-import { 
-    ChevronDownIcon,
-    PlusIcon,
-    MinusIcon
- } from "@heroicons/react/outline";
+import { ChevronDownIcon, PlusIcon, MinusIcon } from "@heroicons/react/outline";
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { DateRangePicker } from "react-date-range";
 
 const Booking = () => {
-  const [datePicker, setDatePicker] = useState(0);
+
+  const [datePicker, setDatePicker] = useState(false);
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [adults, setAdults] = useState(1);
-  const [kids, setKids] = useState(1);
-  
+  const [kids, setKids] = useState(0);
+
   const handleSelect = (ranges) => {
     setStartDate(ranges.selection.startDate);
     setEndDate(ranges.selection.endDate);
@@ -26,20 +23,32 @@ const Booking = () => {
     key: "selection",
   };
 
+  // Decrement number of adults and kids
+  const decrementAdults = () => {
+    setAdults( adults > 1? adults - 1 : adults=1);
+  }
+  const decrementKids = () => {
+    setKids( kids > 1? kids - 1 : kids=0);
+  }
+  // Cancel Booking close Date Range Picker and Number of Guest inputs
+  const cancelBookinn = () => {
+    setDatePicker(false);
+  }
+
   return (
-    <div className="p-5 flex flex-col gap-3">
+    <div className="p-5 flex flex-col gap-2">
       <h2 className="text-xl font-bold"> HAGA SU RESERVACIÓN </h2>
 
       <div className="flex justify-between">
-        <label
-          className="flex gap-5 border-2 rounded-lg px-2 py-1"
+        <span
+          className="flex gap-5 border-2 border-purple-800 rounded-lg px-3"
           onClick={() => setDatePicker(!datePicker)}
         >
           Check in <ChevronDownIcon className="h-6 cursor-pointer" />{" "}
-        </label>
-        <label className="flex gap-5 border-2 rounded-lg px-2 py-1">
+        </span>
+        <span className="flex gap-5 border-2 border-purple-800 rounded-lg px-3">
           Check out <ChevronDownIcon className="h-6 cursor-pointer" />{" "}
-        </label>
+        </span>
       </div>
 
       {datePicker && (
@@ -51,41 +60,54 @@ const Booking = () => {
             rangeColors={["#6A0DAD"]}
             onChange={handleSelect}
           />
+          <div className="flex justify-between items-center border-b border-purple-800 py-2">
           <h2 className="text-xl font-semibold">Huéspedes</h2>
-          <div className="flex justify-between">
-            <div>
-                <label htmlFor="adults">Adultos</label>
-                <div className="flex items-center">
-                    <PlusIcon className="h-5 cursor-pointer" onClick={() => setAdults(adults+=1)}/>
-                    <input 
-                        type="number" 
-                        name="guests" 
-                        id="adults" 
-                        value={adults} 
-                        className="outline-none input::-webkit-outer-soin-button"
-                    />
-                    <MinusIcon className="h-5 cursor-pointer" onClick={() => {setAdults(adults-=1)}}/>
-                </div>
+            <p>Adultos</p>
+            <div className="flex">
+              <div className="flex items-center gap-5">
+                <PlusIcon
+                  className="h-6 cursor-pointer border-2 border-purple-800 rounded-full p-1"
+                  onClick={() => setAdults((adults += 1))}
+                />
+                <span className="text-gray-500 font-semibold">{adults}</span>
+                <MinusIcon
+                  className="h-6 cursor-pointer border-2 border-purple-800 rounded-full p-1"
+                  onClick={decrementAdults}
+                />
+              </div>
             </div>
+            <p>Niños</p>
             <div>
-                <label htmlFor="kids">Niños</label>
-                <div className="flex items-center">
-                    <PlusIcon className="h-5 cursor-pointer" onClick={() => setKids(kids+=1)}/>
-                    <input 
-                        type="number" 
-                        name="guests" 
-                        id="kids" 
-                        value={kids}
-                        className="outline-none"
-                    />
-                    <MinusIcon className="h-5 cursor-pointer" onClick={() => setKids(kids-=1)}/>
-                </div>
+              <div className="flex items-center gap-5">
+                <PlusIcon
+                  className="h-6 cursor-pointer border-2 border-purple-800 rounded-full p-1"
+                  onClick={() => setKids((kids += 1))}
+                />
+                <span className="font-semibold">{kids}</span>
+                <MinusIcon
+                  className="h-6 cursor-pointer border-2 border-purple-800 rounded-full p-1"
+                  onClick={decrementKids}
+                />
+              </div>
             </div>
+          </div>
+          <div className="flex">
+            <button 
+              className="flex-grow text-gray-500"
+              onClick={cancelBookinn}
+            >
+              Cancel
+            </button>
+            <button 
+              className="flex-grow text-purple-800"
+            >
+              Continue
+            </button>
           </div>
         </>
       )}
 
-      <button className="bg-purple-800 text-white rounded-lg p-1">
+      <button className="bg-purple-800 text-white rounded-lg p-1 mt-1">
         RESERVAR
       </button>
     </div>
